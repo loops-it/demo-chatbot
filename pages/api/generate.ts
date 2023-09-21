65ac66eb73a959aae8413aa5ecc9b609d84fe33f
tj <thinupavani@gmail.com>
2023-09-21 14:51:15 +0530

    update

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -30,6 +30,7 @@ export default async function (req: { body: { user_Message: string; }; }, res: {
       model: "text-davinci-003",
       prompt: `Friendly reply to: ${userQuestion} and do Not ask any questions.`,
       temperature: 0.6,
+      max_tokens: 512,
     });
     // console.log("bot : ",completion.data.choices[0])
     res.status(200).json({ result: completion.data.choices[0].text });
